b2d9efad86a25b150bb72e1326e7f7b3eefbfc5e
cjbergmen <cjbergmen@gmail.com>
2026-06-20 13:34:16 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '20260620-prayer-ring-top-constant';
+const VERSION = '20260620-prayer-ring-scrollfade';
 const CACHE = 'cjf-v' + VERSION;
 
 // Install: skip waiting so new SW activates immediately
